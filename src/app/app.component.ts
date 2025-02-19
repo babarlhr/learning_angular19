b99c9ac14bfddc5d4931da1e45c8fe0a59a9b6e7
ishfaq babar <babarlhr1@gmail.com>
2025-02-19 17:24:59 +0500

    Added and import the variables component to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { VariablesComponent } from './components/variables/variables.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,VariablesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
