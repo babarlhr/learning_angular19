e916239b3aed11ac6b3542c9de278bfd8e3dc859
ishfaq babar <babarlhr1@gmail.com>
2025-02-19 18:15:28 +0500

    Add app-data-binding component to main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { VariablesComponent } from './components/variables/variables.component';
+import { DataBindingComponent } from './components/data-binding/data-binding.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,VariablesComponent],
+  imports: [RouterOutlet,VariablesComponent,DataBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
